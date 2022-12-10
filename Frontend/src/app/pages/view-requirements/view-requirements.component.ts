b843391829700d417281a10784831bcd031341c4
Ambili001 <105069520+Ambili001@users.noreply.github.com>
2022-12-11 01:02:28 +0530

    view

---
+++ Frontend/src/app/pages/view-requirements/view-requirements.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-requirements',
+  templateUrl: './view-requirements.component.html',
+  styleUrls: ['./view-requirements.component.css']
+})
+export class ViewRequirementsComponent {
+
+}
